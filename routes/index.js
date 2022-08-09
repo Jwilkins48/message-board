d94dca5ac5c30d0e2ad96872d057fa95149df1dc
Jenna <jenna48071@gmail.com>
2022-08-09 12:26:32 -0500

    displays message on board

--- routes/index.js
+++ routes/index.js
@@ -14,11 +14,26 @@ const messages = [{
 ]
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', {
     title: 'Mini Message Board',
     messages: messages
   });
 });
+
+router.get('/new', (req, res, next) => {
+  res.render('form', {
+    title: 'Mini Message Board',
+  });
+});
+
+router.post('/new', (req, res, next) => {
+  messages.push({
+    text: req.body.userMessage,
+    user: req.body.userName,
+    added: new Date()
+  });
+  res.redirect('/');
+})
 
 module.exports = router;
