74c8c04724a985dd931221134154d2598dd545d0
Jenna <jenna48071@gmail.com>
2022-08-09 12:06:29 -0500

    added new form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [{
+    text: 'Hello',
+    user: 'Amy',
+    added: new Date()
+  },
+  {
+    text: 'Hi',
+    user: 'Charles',
+    added: new Date()
+  }
+]
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Mini Message Board',
+    messages: messages
+  });
 });
 
 module.exports = router;
